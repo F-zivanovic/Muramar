7ee0bde49d51121acef79e1efa03af524c95e0fe
Filip-Zivanovic <filip.zivanovic1999@gmail.com>
2023-12-05 17:54:13 +0100

    Change Gallery section - Dynamic creation of content

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,12 +11,12 @@ hamburgerIcon.addEventListener('click', () => {
 // Close navigation list when click on link of navigation list
 const mobileNavItems = document.querySelectorAll('.nav__list > .nav__item');
 
-mobileNavItems.forEach(function(el){
-    el.addEventListener('click', function(){
+mobileNavItems.forEach(function (el) {
+    el.addEventListener('click', function () {
         navList.classList.remove('active');
         hamburgerIcon.classList.remove('open');
     })
-   
+
 });
 
 
@@ -29,9 +29,9 @@ window.addEventListener('scroll', () => {
 
 
 // Close navigation list when we resize window
-window.addEventListener('resize', (e) =>{
+window.addEventListener('resize', (e) => {
 
-    if(window.innerWidth > 768){
+    if (window.innerWidth > 768) {
         navList.classList.remove('active');
         hamburgerIcon.classList.remove('open');
     }
@@ -57,4 +57,42 @@ const btn = document.getElementById('scrollBtn');
 btn.addEventListener('click', () => {
     window.scroll({ top: 0, behavior: "smooth" });
 });
+
+
+// Gallery section
+const images = [
+    'assets/images/Raftingfoto(25).jpg',
+    'assets/images/Raftingfoto(26).jpg',
+    'assets/images/Raftingfoto(27).jpg',
+    'assets/images/Raftingfoto(28).jpg',
+    'assets/images/Raftingfoto(29).jpg',
+    'assets/images/Raftingfoto(30).jpg',
+    'assets/images/Raftingfoto(31).jpg',
+    'assets/images/Raftingfoto(32).jpg',
+    'assets/images/Raftingfoto(33).jpg',
+    'assets/images/Raftingfoto(35).jpg',
+    'assets/images/Raftingfoto(36).jpg',
+    'assets/images/Raftingfoto(37).jpg',
+    'assets/images/Raftingfoto(38).jpg',
+    'assets/images/Raftingfoto(39).jpg',
+    'assets/images/Raftingfoto(40).jpg',
+    'assets/images/Raftingfoto(41).jpg',
+    'assets/images/Raftingfoto(43).jpg',
+    'assets/images/Raftingfoto(44).jpg'
+];
+
+window.addEventListener('load', () => {
+    const gallery = document.querySelector('.gallery__wrapper');
+
+
+    for (let i = 0; i < images.length; i++) {
+        gallery.innerHTML += `      
+                <div class="gallery__img">
+                    <img src="${ images[i] }" alt="gallery img">
+                </div>`
+    }
+});
+
+
+
 
